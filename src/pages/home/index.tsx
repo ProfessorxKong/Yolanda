f9a66195df89835e2e1a87f2b61d703f50729103
ProfessorxKong <seiiiiin0102@gmail.com>
2025-08-11 18:01:55 +0800

    feat: add background SVG and restructure HomePage component with new styles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,13 @@
+import React from 'react'
 import styles from './index.module.scss'
+import Background from '@/assets/svg/background.svg?react'
 
-const HomePage = () => {
-  return <div className={styles['home-page']}>HomePage</div>
+const HomePage = (): React.ReactElement => {
+  return (
+    <div className={styles['home-page']}>
+      <Background className={styles['background']} />
+    </div>
+  )
 }
 
 export default HomePage
--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        jsxRuntime: 'automatic',
-        svgo: true,
-        svgoConfig: { plugins: ['preset-default'] },
-      },
-      include: '**/*.svg',
-    }),
-  ],
+  plugins: [svgr(), react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
